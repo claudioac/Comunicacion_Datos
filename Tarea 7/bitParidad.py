0ac3d596440d90d88dddbbce900a8291ce2c9f38
ljofre <ljofre2146@gmail.com>
2013-11-13 11:18:44 -0300

    bit de paridad

---
+++ Tarea 7/bitParidad.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""bit de paridad"""
+
+def paridadFlag(trama):
+    bit = sum(trama)
+    return int(not mod(bit,2))
+
+#ejemplo
+print "ejemplos : \n"
+trama = [0,1]
+
+print "el bit de paridad para ", trama, " es ",paridadFlag(trama=trama)
+
+
+trama = [0,1,1]
+print "el bit de paridad para ", trama, " es ",paridadFlag(trama=trama)
+
+
+
+
